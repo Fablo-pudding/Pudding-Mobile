cdc0d729fcaf7711b822ed17065241272adfee61
정지윤 <163826760+jiyun040@users.noreply.github.com>
2025-09-04 15:14:25 +0900

    BottomNavigationBar 추가

---
+++ lib/core/components/bottom_navigation_bar.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:material_symbols_icons/material_symbols_icons.dart';
+import 'package:pudding/core/constants/color.dart';
+
+class CustomBottomNavigationBar extends StatelessWidget {
+  final int currentIndex;
+  final void Function(int) onTap;
+
+  const CustomBottomNavigationBar({
+    super.key,
+    required this.currentIndex,
+    required this.onTap,
+  });
+
+  static const icons = [
+    Symbols.home,
+    Symbols.chat_bubble,
+    Symbols.crown,
+    Symbols.kitchen,
+    Symbols.person,
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        color: PuddingColor.background,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(8),
+          topRight: Radius.circular(8),
+        ),
+      ),
+      child: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 14),
+          child: Row(
+            children: List.generate(icons.length, (index) {
+              final isSelected = currentIndex == index;
+
+              return Expanded(
+                child: GestureDetector(
+                  onTap: () => onTap(index),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Icon(
+                        icons[index],
+                        fill: 1,
+                        color: isSelected
+                            ? PuddingColor.brown
+                            : PuddingColor.gray400,
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            }),
+          ),
+        ),
+      ),
+    );
+  }
+}
