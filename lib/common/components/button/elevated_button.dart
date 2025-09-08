29513fecafa788c52280b0e0b028549eb153ea6c
최승우 <choi0250655@gmail.com>
2025-09-09 01:02:35 +0900

    ElevatedButton 수정
    required, string text에서 Widget contents로 수정

--- lib/common/components/button/elevated_button.dart
+++ lib/common/components/button/elevated_button.dart
@@ -4,13 +4,13 @@ import 'package:pudding/common/constants/color.dart';
 import 'package:pudding/common/constants/text_style.dart';
 
 class PuddingElevatedButton extends StatelessWidget {
-  final String text;
+  final Widget contents;
   final void Function()? onPressed;
 
   const PuddingElevatedButton({
     super.key,
-    required this.text,
-    this.onPressed,
+    required this.contents,
+    required this.onPressed,
   });
 
   @override
@@ -39,7 +39,7 @@ class PuddingElevatedButton extends StatelessWidget {
         textStyle: WidgetStateProperty.all(PuddingTextStyle.button),
         minimumSize: WidgetStateProperty.all(Size(80, 48)),
       ),
-      child: Text(text),
+      child: contents,
     );
   }
 }
