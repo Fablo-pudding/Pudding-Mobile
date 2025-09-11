04e4d09417b8e38e3f096d4f29238399e86fa2d4
최승우 <choi0250655@gmail.com>
2025-09-11 14:13:02 +0900

    tabBar, tabBarView 추가
    tabBar, tabBarView 추가

---
+++ lib/common/components/tab_bar.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:pudding/common/constants/color.dart';
+import 'package:pudding/common/constants/text_style.dart';
+
+class PuddingTab extends StatefulWidget {
+  const PuddingTab({super.key});
+
+  @override
+  State<PuddingTab> createState() => _PuddingTabState();
+}
+
+class _PuddingTabState extends State<PuddingTab>
+    with SingleTickerProviderStateMixin{
+  late TabController controller;
+
+  @override
+  void initState(){
+    super.initState();
+    controller = TabController(length: 2, vsync: this);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return  Column(
+      children: [
+        TabBar(
+          labelColor: PuddingColor.brown,
+          unselectedLabelColor: PuddingColor.gray400,
+          labelStyle: PuddingTextStyle.heading2,
+          indicatorColor: PuddingColor.brown,
+          indicatorWeight: 1,
+          indicatorSize: TabBarIndicatorSize.tab,
+          controller: controller,
+          tabs: <Widget>[
+            Tab(child: Text('로그인'),),
+            Tab(child: Text('회원가입'),),
+          ],
+        ),
+        Expanded(
+          child: TabBarView(
+            controller: controller,
+            children: const [
+              Center(child: Text('첫번째 페이지')),
+              Center(child: Text('두번째 페이지')),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
