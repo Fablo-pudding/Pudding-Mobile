657e77d2206dc58f0c75fd200375adcdbabe88d1
정지윤 <163826760+jiyun040@users.noreply.github.com>
2025-09-16 21:55:34 +0900

    스톱워치 버튼 추가

---
+++ lib/views/main_page/components/stopwatch.dart
@@ -0,0 +1,62 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:pudding/common/constants/color.dart';
+import 'package:pudding/common/constants/text_style.dart';
+
+class PuddingStopWatch extends StatefulWidget {
+  final void Function(int seconds)? onTick;
+
+  const PuddingStopWatch({
+    Key? key,
+    this.onTick,
+  }) : super(key: key);
+
+  @override
+  State<PuddingStopWatch> createState() => _PuddingStopWatchState();
+}
+
+class _PuddingStopWatchState extends State<PuddingStopWatch> {
+  bool isRunning = false;
+  int _seconds = 0;
+  Timer? _timer;
+
+  void _start() {
+    setState(() => isRunning = true);
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      setState(() => _seconds++);
+      widget.onTick?.call(_seconds);
+    });
+  }
+
+  void _stop() {
+    setState(() => isRunning = false);
+    _timer?.cancel();
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final buttonText = isRunning ? '공부 중단하기' : '공부 시작하기';
+
+    return ElevatedButton(
+      onPressed: isRunning ? _stop : _start,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: PuddingColor.brown,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+      ),
+      child: Text(
+        buttonText,
+        style: PuddingTextStyle.body1.copyWith(
+          color: PuddingColor.background,
+        ),
+      ),
+    );
+  }
+}
