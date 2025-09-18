2561e3e4f408837a29112f3354d04e0d00799a62
정지윤 <163826760+jiyun040@users.noreply.github.com>
2025-09-18 18:36:24 +0900

    확인, 취소 모달 추가 (#8)
    * 확인, 취소 모달 추가
    
    * 텍스트 스타일 오타 수정
    
    * 확인, 취소 모달 수정
    
    * 콜백 함수 타입 수정

--- lib/common/constants/text_style.dart
+++ lib/common/constants/text_style.dart
@@ -4,7 +4,7 @@ import 'package:pudding/common/constants/color.dart';
 class PuddingTextStyle{
   static const TextStyle NanumBarunpenB = TextStyle(fontFamily: 'NanumBerunpenB', color: PuddingColor.brown);
   static const TextStyle NanumBarunpenR = TextStyle(fontFamily: 'NanumBarunpenR', color: PuddingColor.brown);
-  static const TextStyle LotteriaChab = TextStyle(fontFamily: 'LiaSans-Bold', color: PuddingColor.brown);
+  static const TextStyle LotteriaChab = TextStyle(fontFamily: 'RiaSans', color: PuddingColor.brown);
 
   /// Heading
   static TextStyle heading1  = NanumBarunpenB.copyWith(
