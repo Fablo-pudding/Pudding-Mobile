e3e2efec1ef2b35a32b62e73fdcac9048e8a6957
정지윤 <163826760+jiyun040@users.noreply.github.com>
2025-09-23 18:59:23 +0900

    feat ::  모달 구현 (#29)
    * 아이템 획득 모달 추가 (#7)
    
    * 아이템 획득 모달 추가
    
    * ItemModal 오타 수정
    
    * 확인, 취소 모달 추가 (#8)
    
    * 확인, 취소 모달 추가
    
    * 텍스트 스타일 오타 수정
    
    * 확인, 취소 모달 수정
    
    * 콜백 함수 타입 수정

---
+++ lib/common/components/modal/check_modal.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:pudding/common//constants/color.dart';
+import 'package:pudding/common//constants/text_style.dart';
+
+class CheckModal extends StatelessWidget {
+  final String message;
+  final void Function()? onConfirm;
+
+  const CheckModal({
+    super.key,
+    required this.message,
+    required this.onConfirm
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      backgroundColor: PuddingColor.background,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8),
+      ),
+      insetPadding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const SizedBox(height: 50),
+          Text(
+            message,
+            style: PuddingTextStyle.heading2.copyWith(
+              color: PuddingColor.black,
+            ),
+          ),
+          const SizedBox(height: 50),
+          const Divider(thickness: 1, height: 1, color: PuddingColor.gray400),
+          IntrinsicHeight(
+            child: Row(
+              children: [
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () => Navigator.of(context).pop(),
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(vertical: 12),
+                      child: Text(
+                        '취소',
+                        textAlign: TextAlign.center,
+                        style: PuddingTextStyle.button.copyWith(
+                          color: PuddingColor.red,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(width: 1, color: PuddingColor.gray400),
+                Expanded(
+                  child: GestureDetector(
+                    onTap: onConfirm,
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(vertical: 12),
+                      child: Text(
+                        '확인',
+                        textAlign: TextAlign.center,
+                        style: PuddingTextStyle.button.copyWith(
+                          color: PuddingColor.brown,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/common/components/modal/item_modal.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:pudding/common/constants/color.dart';
+import 'package:pudding/common/constants/text_style.dart';
+import 'package:flutter_svg/svg.dart';
+
+class ItemModal extends StatelessWidget {
+  final String svgPath;
+
+  const ItemModal({
+    super.key,
+    required this.svgPath
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      backgroundColor: PuddingColor.background,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(const Radius.circular(8)),
+      ),
+      insetPadding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 18),
+            child: Text(
+              '아이템을 획득하였습니다.',
+              style: PuddingTextStyle.heading3.copyWith(
+                color: PuddingColor.brown,
+              ),
+            ),
+          ),
+          SvgPicture.asset(svgPath),
+          const SizedBox(height: 20),
+          Divider(color: PuddingColor.gray400, thickness: 1),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 13),
+            child: GestureDetector(
+              onTap: () {
+                Navigator.pop(context);
+              },
+              child: Text(
+                '확인',
+                style: PuddingTextStyle.body1.copyWith(
+                  color: PuddingColor.black,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
