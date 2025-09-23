7ba7d82660b00ff2e4d4cba86d76a655602ee455
정지윤 <163826760+jiyun040@users.noreply.github.com>
2025-09-23 20:49:38 +0900

    메인화면 뷰 수정

--- lib/views/main_page/presentation/main_page.dart
+++ lib/views/main_page/presentation/main_page.dart
@@ -1,67 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:pudding/common/components/button/elevated_button.dart';
 import 'package:pudding/common/constants/color.dart';
 import 'package:pudding/common/constants/text_style.dart';
 import 'package:pudding/views/main_page/components/study_control.dart';
 
 class PuddingMainPage extends StatelessWidget {
-  final double? height;
-
-  const PuddingMainPage({super.key, this.height});
+  const PuddingMainPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          const SizedBox(height: 72),
-          StudyControlBanner(),
-          const SizedBox(height: 16),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Container(
-              width: double.infinity,
-              height: height ?? 68,
-              decoration: BoxDecoration(
-                color: PuddingColor.main,
-                borderRadius: BorderRadius.circular(12),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 20,
-                  vertical: 10,
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      '모르는 것이 있나요?',
-                      style: PuddingTextStyle.heading3.copyWith(
-                        color: PuddingColor.brown,
-                      ),
-                    ),
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: PuddingColor.main,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12),
-                          side: BorderSide(color: PuddingColor.brown),
-                        ),
-                        elevation: 0,
-                      ),
-                      onPressed: () {},
-                      child: Text(
-                        '물어보러가기',
-                        style: PuddingTextStyle.body1.copyWith(
-                          color: PuddingColor.brown,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            const SizedBox(height: 16),
+            const StudyControlBanner(),
+            const SizedBox(height: 16),
+            const _QuestionBanner(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _QuestionBanner extends StatelessWidget {
+  const _QuestionBanner();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Container(
+        width: double.infinity,
+        height: 68,
+        decoration: BoxDecoration(
+          color: PuddingColor.main,
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 10,
           ),
-        ],
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                '모르는 것이 있나요?',
+                style: PuddingTextStyle.heading3.copyWith(
+                  color: PuddingColor.brown,
+                ),
+              ),
+              PuddingElevatedButton(
+                onPressed: () {},
+                background: PuddingColor.main,
+                child: Text(
+                  '물어보러가기',
+                  style: PuddingTextStyle.body1.copyWith(
+                    color: PuddingColor.brown,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
