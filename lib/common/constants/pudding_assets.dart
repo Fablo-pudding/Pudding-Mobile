b8dd23478dd2b7e12f4246fc79fd9f8930de28bc
정지윤 <163826760+jiyun040@users.noreply.github.com>
2025-09-29 15:37:09 +0900

    asset 클래스로 따로 분리

---
+++ lib/common/constants/pudding_assets.dart
@@ -0,0 +1,7 @@
+abstract final class PuddingAssets {
+  static const egg = 'assets/img/egg.svg';
+  static const milk = 'assets/img/milk.svg';
+  static const sugar = 'assets/img/sugar.svg';
+  static const pudding = 'assets/img/pudding.svg';
+  static const logo = 'assets/img/logo.svg';
+}
