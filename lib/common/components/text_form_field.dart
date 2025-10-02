610159cb74747e46c63384b78f6efd4c2a3e4a79
최승우 <choi0250655@gmail.com>
2025-10-02 18:32:51 +0900

    textformfield
    textformfield

--- lib/common/components/text_form_field.dart
+++ lib/common/components/text_form_field.dart
@@ -1,201 +1,107 @@
 import 'package:flutter/material.dart';
 import 'package:pudding/common/constants/color.dart';
 import 'package:pudding/common/constants/text_style.dart';
-import 'package:material_symbols_icons/material_symbols_icons.dart';
 
-enum PuddingTextFormFieldType { signup, signin, write, comment }
 
-class PuddingTextFormField extends StatefulWidget {
-  final String hintText;
+class PuddingTextFormField extends StatelessWidget {
   final String? title;
-  final PuddingTextFormFieldType textFormField;
+  final bool? errorIcon;
+  final bool pwObsText;
+  final TextEditingController controller;
+  final TextStyle? style;
+  final TextAlign? textAlign;
+  final InputDecoration? decoration;
+  final void Function(String)? onchanged;
+  final FormFieldValidator<String>? validator;
+  final IconButton? suffixIcon;
+  final IconButton? prefixIcon;
+  final Color? cursorColor;
+  final bool autocrrect;
+  final TextInputType? keyboradType;
+  final int? maxLength;
+  final bool choiceEnableBorders;
+  final void Function(PointerDownEvent)? onTapOutSide;
 
   const PuddingTextFormField({
     super.key,
-    required this.hintText,
     this.title,
-    required this.textFormField,
+    this.errorIcon = false,
+    this.pwObsText = false,
+    required this.controller,
+    this.style,
+    this.textAlign,
+    this.decoration = const InputDecoration(),
+    this.onchanged,
+    this.validator,
+    this.suffixIcon,
+    this.prefixIcon,
+    this.cursorColor,
+    required this.autocrrect,
+    this.keyboradType,
+    this.maxLength,
+    this.choiceEnableBorders = true,
+    this.onTapOutSide,
   });
-
-  @override
-  State<PuddingTextFormField> createState() => _PuddingTextFormFieldState();
-}
-
-class _PuddingTextFormFieldState extends State<PuddingTextFormField> {
-  final _key = GlobalKey<FormState>();
-  bool errorIcon = false;
-  bool pwObsText = true;
-  final signupController = TextEditingController();
-  final signinpController = TextEditingController();
-  final writeController = TextEditingController();
-  final commentController = TextEditingController();
-
-  @override
-  void dispose() {
-    signupController.dispose();
-    signinpController.dispose();
-    writeController.dispose();
-    commentController.dispose();
-    super.dispose();
-  }
-
-  final enableBorder = OutlineInputBorder(
-    borderRadius: BorderRadius.circular(8),
-    borderSide: BorderSide(color: PuddingColor.white, width: 1),
-  );
-
-  final focusBorder = OutlineInputBorder(
-    borderRadius: BorderRadius.circular(8),
-    borderSide: BorderSide(color: PuddingColor.black, width: 1),
-  );
-
-  final cousorColor = PuddingColor.brown;
-
-  Widget puddingEnum(PuddingTextFormFieldType type) {
-    switch (type) {
-      case PuddingTextFormFieldType.signup:
-        return TextFormField(
-          style: PuddingTextStyle.body1,
-          cursorColor: cousorColor,
-          autovalidateMode: AutovalidateMode.onUserInteraction,
-          controller: signupController,
-          validator: (value) {
-            if (value == null || value.isEmpty) {
-              return '아이디를 입력해주세요';
-            }
-            return null;
-          },
-          onChanged: (value) {
-            setState(() {
-              errorIcon = value.isEmpty;
-            });
-          },
-          decoration: InputDecoration(
-            enabledBorder: enableBorder,
-            focusedBorder: focusBorder,
-            errorBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: PuddingColor.white),
-            ),
-            focusedErrorBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: PuddingColor.white),
-            ),
-            errorStyle: PuddingTextStyle.body3.copyWith(color: Colors.red),
-            filled: true,
-            fillColor: PuddingColor.white,
-            hintText: widget.hintText,
-            hintStyle: PuddingTextStyle.body1.copyWith(
-              color: PuddingColor.gray400,
-            ),
-            suffixIcon: errorIcon
-                ? IconButton(
-                    onPressed: () {
-                      signupController.clear();
-                    },
-                    icon: Icon(Symbols.cancel, color: Colors.red),
-                  )
-                : null,
-          ),
-        );
-      case PuddingTextFormFieldType.signin:
-        return TextFormField(
-          style: PuddingTextStyle.body1,
-          cursorColor: cousorColor,
-          obscureText: pwObsText,
-          autovalidateMode: AutovalidateMode.onUserInteraction,
-          controller: signinpController,
-          decoration: InputDecoration(
-            enabledBorder: enableBorder,
-            focusedBorder: focusBorder,
-            errorStyle: PuddingTextStyle.body3.copyWith(color: Colors.red),
-            filled: true,
-            fillColor: PuddingColor.white,
-            hintText: widget.hintText,
-            hintStyle: PuddingTextStyle.body1.copyWith(
-              color: PuddingColor.gray400,
-            ),
-            suffixIcon: IconButton(
-              onPressed: () => setState(() => pwObsText = !pwObsText),
-              icon: Icon(
-                pwObsText
-                    ? Icons.visibility_outlined
-                    : Icons.visibility_off_outlined,
-              ),
-            ),
-          ),
-        );
-      case PuddingTextFormFieldType.write:
-        return Container(
-          decoration: BoxDecoration(
-            border: Border(
-              top: BorderSide(color: PuddingColor.brown, width: 1),
-              bottom: BorderSide(color: PuddingColor.brown, width: 1),
-            ),
-          ),
-          child: TextFormField(
-            cursorColor: cousorColor,
-            controller: writeController,
-            style: PuddingTextStyle.heading2,
-            textAlign: TextAlign.center,
-            decoration: InputDecoration(
-              contentPadding: EdgeInsets.symmetric(vertical: 15.5,horizontal: 60),
-              border: InputBorder.none,
-              filled: true,
-              fillColor: PuddingColor.background,
-              prefixIcon: IconButton(
-                onPressed: () {},
-                icon: Icon(Symbols.close,color: PuddingColor.brown,),
-                padding: EdgeInsets.symmetric(horizontal: 20),
-              ),
-              hintText: widget.hintText,
-              hintStyle: PuddingTextStyle.heading2.copyWith(
-                color: PuddingColor.gray400,
-              ),
-            ),
-          ),
-        );
-      case PuddingTextFormFieldType.comment:
-        return TextFormField(
-          cursorColor: cousorColor,
-          controller: commentController,
-          style: PuddingTextStyle.body1,
-          decoration: InputDecoration(
-            contentPadding: EdgeInsets.symmetric(vertical: 13.5,horizontal: 20),
-            enabledBorder: enableBorder,
-            focusedBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(8),
-              borderSide: BorderSide(color: PuddingColor.white, width: 1),
-            ),
-            suffixIcon: IconButton(
-              onPressed: () {},
-              icon: Icon(
-                Symbols.keyboard_arrow_up,
-                color: PuddingColor.gray400,
-              ),
-            ),
-            filled: true,
-            fillColor: PuddingColor.white,
-            hintText: widget.hintText,
-            hintStyle: PuddingTextStyle.body2.copyWith(
-              color: PuddingColor.gray400,
-            ),
-          ),
-        );
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _key,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          if (widget.title != null)
-            Text(widget.title!, style: PuddingTextStyle.heading3),
-          const SizedBox(height: 8),
-          puddingEnum(widget.textFormField),
-        ],
-      ),
+    final defaultTextStyle = PuddingTextStyle.body1;
+    final enableBorder = OutlineInputBorder(
+      borderRadius: BorderRadius.circular(8),
+      borderSide: BorderSide(color: PuddingColor.white, width: 1),
+    );
+   final enableBorder2 = Container(
+     decoration: BoxDecoration(
+       border: Border(
+         top: BorderSide(color: PuddingColor.brown,width: 1),
+         bottom:  BorderSide(color: PuddingColor.brown,width: 1),
+       )
+     ),
+   );
+
+final enableBorders = choiceEnableBorders ? enableBorder : enableBorder2;
+    final focusBorder = OutlineInputBorder(
+      borderRadius: BorderRadius.circular(8),
+      borderSide: BorderSide(color: PuddingColor.black, width: 1),
+    );
+    final cousorColor = PuddingColor.brown;
+
+    InputDecoration? inputDecoration = InputDecoration(
+      hintText: decoration?.hintText,
+      hintStyle: decoration?.hintStyle,
+      errorText: decoration?.errorText,
+      errorStyle: decoration?.errorStyle,
+      icon: decoration?.icon,
+      suffixIcon: decoration?.suffixIcon,
+      prefixIcon: decoration?.prefixIcon,
+      suffixStyle: decoration?.suffixStyle,
+      prefixStyle: decoration?.prefixStyle,
+      enabledBorder: enableBorder,
+      focusedBorder: focusBorder,
+      focusColor: decoration?.focusColor,
+      fillColor: decoration?.fillColor,
+      filled: true,
+    );
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        if(title!= null)
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Text(title!,style: PuddingTextStyle.heading3,),
+          ),
+        TextFormField(
+          decoration: inputDecoration,
+          style: defaultTextStyle,
+          controller: controller,
+          autocorrect: autocrrect,
+          keyboardType: keyboradType,
+          obscureText: pwObsText,
+          validator: validator,
+          cursorColor: cousorColor,
+          maxLength: maxLength,
+          onTapOutside:  (event) => FocusScope.of(context).unfocus(),
+        ),
+      ],
     );
   }
 }
