09d796db897fc00dc0e89903fcc1cda82c73ec86
정지윤 <163826760+jiyun040@users.noreply.github.com>
2025-10-10 14:53:21 +0900

    feat :: 앱바 추가 (#13)
    * 앱바 추가
    
    * 앱바 클래스 사용으로 수정
    
    * 앱바 수정
    
    - leading으로 수정
    - preferredSize 사용
    
    * 앱바 actions 삭제

---
+++ lib/common/components/app_bar/app_bar.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:pudding/common/constants/color.dart';
+import 'package:pudding/common/constants/text_style.dart';
+
+class PuddingAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final Widget? leading;
+  final String title;
+  final TextStyle? titleStyle;
+
+  const PuddingAppBar({
+    super.key,
+    this.leading,
+    required this.title,
+    this.titleStyle,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return PreferredSize(
+      preferredSize: preferredSize,
+      child: AppBar(
+        backgroundColor: PuddingColor.background,
+        elevation: 0,
+        title: Text(
+          title,
+          style:
+              titleStyle ??
+              PuddingTextStyle.lotteriaChab.copyWith(color: PuddingColor.brown),
+        ),
+        leading: leading,
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(54);
+}
