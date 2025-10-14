daedbe222b5d39242ab76a7c1ef4ba0bd5bfbdac
최승우 <choi0250655@gmail.com>
2025-10-14 19:21:12 +0900

    gender_button 수정
    gender_button 수정

--- lib/common/components/button/gender_button.dart
+++ lib/common/components/button/gender_button.dart
@@ -3,37 +3,33 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:pudding/common/constants/color.dart';
 
 class PuddingGenderButton extends StatefulWidget {
-  final String image;
   final void Function() onTap;
-  const PuddingGenderButton({
-    super.key,
-    required this.image,
-    required this.onTap,
-  });
+  const PuddingGenderButton({super.key, required this.onTap});
 
   @override
   State<PuddingGenderButton> createState() => _PuddingGenderButtonState();
 }
 
 class _PuddingGenderButtonState extends State<PuddingGenderButton> {
-   bool manSelected = false;
-   bool womanSelcted = false;
+  bool manSelected = false;
+  bool womanSelcted = false;
 
-   void manselted(){
-     setState(() {
-       manSelected = true;
-       womanSelcted = false;
-       widget.onTap();
-     });
-}
+  void manselted() {
+    setState(() {
+      manSelected = true;
+      womanSelcted = false;
+      widget.onTap();
+    });
+  }
 
-   void girlselted(){
-     setState(() {
-       manSelected = false;
-       womanSelcted = true;
-       widget.onTap();
-     });
-   }
+  void girlselted() {
+    setState(() {
+      manSelected = false;
+      womanSelcted = true;
+      widget.onTap();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -45,7 +41,7 @@ class _PuddingGenderButtonState extends State<PuddingGenderButton> {
               EdgeInsets.symmetric(vertical: 53, horizontal: 44),
             ),
             backgroundColor: WidgetStateProperty.all(
-              manSelected ? PuddingColor.main : PuddingColor.background
+              manSelected ? PuddingColor.main : PuddingColor.background,
             ),
             shape: WidgetStateProperty.all(
               RoundedRectangleBorder(
@@ -53,37 +49,29 @@ class _PuddingGenderButtonState extends State<PuddingGenderButton> {
                 side: BorderSide(color: PuddingColor.brown, width: 1),
               ),
             ),
-            minimumSize: WidgetStateProperty.all(Size(60, 80))
+            minimumSize: WidgetStateProperty.all(Size(60, 80)),
           ),
-          child: Row(
-            children: [
-              SvgPicture.asset('assets/img/man.svg'),
-            ],
-          ),
+          child: Row(children: [SvgPicture.asset('assets/img/man.svg')]),
         ),
-        SizedBox(width: 31,),
+        SizedBox(width: 31),
         ElevatedButton(
           onPressed: girlselted,
           style: ButtonStyle(
-              padding: WidgetStateProperty.all(
-                EdgeInsets.symmetric(vertical: 53, horizontal: 44),
+            padding: WidgetStateProperty.all(
+              EdgeInsets.symmetric(vertical: 53, horizontal: 44),
+            ),
+            backgroundColor: WidgetStateProperty.all(
+              womanSelcted ? PuddingColor.main : PuddingColor.background,
+            ),
+            shape: WidgetStateProperty.all(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12),
+                side: BorderSide(color: PuddingColor.brown, width: 1),
               ),
-              backgroundColor: WidgetStateProperty.all(
-                  womanSelcted ? PuddingColor.main : PuddingColor.background
-              ),
-              shape: WidgetStateProperty.all(
-                RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12),
-                  side: BorderSide(color: PuddingColor.brown, width: 1),
-                ),
-              ),
-              minimumSize: WidgetStateProperty.all(Size(60, 80))
+            ),
+            minimumSize: WidgetStateProperty.all(Size(60, 80)),
           ),
-          child: Row(
-            children: [
-              SvgPicture.asset('assets/img/woman.svg'),
-            ],
-          ),
+          child: Row(children: [SvgPicture.asset('assets/img/woman.svg')]),
         ),
       ],
     );
