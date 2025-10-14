cb6ab96713e088ad0c47eaf97a51170e94aa8464
최승우 <choi0250655@gmail.com>
2025-10-14 19:23:56 +0900

    dropdownButton 생성 (#22)
    * dropdownButton 구현

---
+++ lib/common/components/button/dropdown_button.dart
@@ -0,0 +1,223 @@
+import 'package:dropdown_button2/dropdown_button2.dart';
+import 'package:flutter/material.dart';
+import 'package:material_symbols_icons/symbols.dart';
+import 'package:pudding/common/constants/color.dart';
+import 'package:pudding/common/constants/text_style.dart';
+
+enum DropdownButtonType { years, months, days }
+
+class PuddingDropdownButton extends StatefulWidget {
+  final DropdownButtonType dropdownButton;
+  const PuddingDropdownButton({super.key, required this.dropdownButton,});
+
+  @override
+  State<PuddingDropdownButton> createState() => _PuddingDropdownButtonState();
+}
+
+class _PuddingDropdownButtonState extends State<PuddingDropdownButton> {
+  final List<int> dropdownYears = List.generate(
+    DateTime.now().year - 2006,
+        (index) => 2006 + index,
+  );
+  final List<int> dropdownMonths = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  final List<int> dropdownDays = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+  ];
+
+  int? dropdownYear;
+  int? dropdownMonth;
+  int? dropdownDay;
+
+  Expanded puddingEnum(DropdownButtonType type) {
+    switch (type) {
+      case DropdownButtonType.years:
+        return Expanded(
+          child: Container(
+            decoration: BoxDecoration(
+              color: PuddingColor.white,
+              borderRadius: BorderRadius.circular(8),
+            ),
+            height: 36,
+            child: DropdownButtonHideUnderline(
+              child: DropdownButton2<int>(
+                menuItemStyleData: MenuItemStyleData(height: 28),
+                dropdownStyleData: DropdownStyleData(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    color: PuddingColor.white,
+                  ),
+                  maxHeight: 124,
+                ),
+                isExpanded: true,
+                isDense: true,
+                hint: Text(
+                  '년도',
+                  style: PuddingTextStyle.body1.copyWith(
+                    color: PuddingColor.gray400,
+                  ),
+                ),
+                value: dropdownYear,
+                iconStyleData: IconStyleData(
+                  icon: Icon(Symbols.arrow_drop_down, size: 24),
+                ),
+                underline: SizedBox.shrink(),
+                style: PuddingTextStyle.body3.copyWith(
+                  color: PuddingColor.black,
+                ),
+                onChanged: (int? newValue) {
+                  setState(() {
+                    dropdownYear = newValue;
+                  });
+                },
+                items: dropdownYears.map((year) {
+                  return DropdownMenuItem<int>(
+                    value: year,
+                    child: Text(
+                      year.toString(),
+                      style: PuddingTextStyle.body1.copyWith(
+                        color: PuddingColor.black,
+                      ),
+                      maxLines: 1,
+                    ),
+                  );
+                }).toList(),
+              ),
+            ),
+          ),
+        );
+      case DropdownButtonType.months:
+        return Expanded(
+          child: Container(
+            decoration: BoxDecoration(
+              color: PuddingColor.white,
+              borderRadius: BorderRadius.circular(8),
+            ),
+            height: 36,
+            child: DropdownButtonHideUnderline(
+              child: DropdownButton2<int>(
+                menuItemStyleData: MenuItemStyleData(height: 30),
+                dropdownStyleData: DropdownStyleData(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    color: PuddingColor.white,
+                  ),
+                  maxHeight: 124,
+                ),
+                isExpanded: true,
+                isDense: true,
+                hint: Text(
+                  '월',
+                  style: PuddingTextStyle.body1.copyWith(
+                    color: PuddingColor.gray400,
+                  ),
+                ),
+                value: dropdownMonth,
+                iconStyleData: IconStyleData(
+                  icon: Icon(Symbols.arrow_drop_down, size: 24),
+                ),
+                underline: SizedBox.shrink(),
+                style: PuddingTextStyle.body1.copyWith(
+                  color: PuddingColor.black,
+                ),
+                onChanged: (int? newValue) {
+                  setState(() {
+                    dropdownMonth = newValue;
+                  });
+                },
+                items: dropdownMonths.map((month) {
+                  return DropdownMenuItem<int>(
+                    value: month,
+                    child: Text(
+                      month.toString(),
+                      style: PuddingTextStyle.body1.copyWith(
+                        color: PuddingColor.black,
+                      ),
+                    ),
+                  );
+                }).toList(),
+              ),
+            ),
+          ),
+        );
+      case DropdownButtonType.days:
+        return Expanded(
+          child: Container(
+            decoration: BoxDecoration(
+              color: PuddingColor.white,
+              borderRadius: BorderRadius.circular(8),
+            ),
+            height: 36,
+            child: DropdownButtonHideUnderline(
+              child: DropdownButton2<int>(
+                menuItemStyleData: MenuItemStyleData(height: 30),
+                dropdownStyleData: DropdownStyleData(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    color: PuddingColor.white,
+                  ),
+                  maxHeight: 124,
+                ),
+                isExpanded: true,
+                isDense: true,
+                hint: Text(
+                  '일',
+                  style: PuddingTextStyle.body1.copyWith(
+                    color: PuddingColor.gray400,
+                  ),
+                ),
+                value: dropdownDay,
+                iconStyleData: IconStyleData(
+                  icon: Icon(Symbols.arrow_drop_down, size: 24),
+                ),
+                underline: SizedBox.shrink(),
+                style: PuddingTextStyle.body1.copyWith(
+                  color: PuddingColor.black,
+                ),
+                onChanged: (int? newValue) {
+                  setState(() {
+                    dropdownDay = newValue;
+                  });
+                },
+                items: dropdownDays.map((day) {
+                  return DropdownMenuItem<int>(
+                    value: day,
+                    child: Text(
+                      day.toString(),
+                      style: PuddingTextStyle.body1.copyWith(
+                        color: PuddingColor.black,
+                      ),
+                    ),
+                  );
+                }).toList(),
+              ),
+            ),
+          ),
+        );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    int dayChoice() {
+      if (dropdownMonth == 2) {
+        if ((dropdownYear! % 4 == 0 && dropdownYear! % 100 != 0) ||
+            dropdownYear! % 400 == 0) {
+          return 29;
+        }
+        return 28;
+      }
+      if (dropdownMonth == 4 || dropdownMonth == 6 || dropdownMonth == 9 || dropdownMonth == 11) {
+        return 30;
+      } else {
+        return 31;
+      }
+    }
+    if ((dropdownDay ?? 0) > dayChoice()) {
+      dropdownDay = null;
+    }
+    return Row(
+      children: [
+        puddingEnum(widget.dropdownButton),
+      ],
+    );
+  }
+}
