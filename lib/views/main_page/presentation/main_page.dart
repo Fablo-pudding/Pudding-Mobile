b785383f25c61c22bdb6b5d378dc91078213f023
정지윤 <163826760+jiyun040@users.noreply.github.com>
2025-10-14 11:09:15 +0900

    feat :: 메인 화면 전체 뷰 구현
    * 메인 화면 전체 뷰 구현
    
    * 메인화면 뷰 수정
    
    * column 내부 spacing 으로 수정

---
+++ lib/views/main_page/presentation/main_page.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:pudding/common/components/button/elevated_button.dart';
+import 'package:pudding/common/constants/color.dart';
+import 'package:pudding/common/constants/text_style.dart';
+import 'package:pudding/views/main_page/components/study_control.dart';
+
+class PuddingMainPage extends StatelessWidget {
+  const PuddingMainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          spacing: 16,
+          children: const [
+            StudyControlBanner(),
+            _QuestionBanner(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _QuestionBanner extends StatelessWidget {
+  const _QuestionBanner();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Container(
+        width: double.infinity,
+        height: 68,
+        decoration: BoxDecoration(
+          color: PuddingColor.main,
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 10,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                '모르는 것이 있나요?',
+                style: PuddingTextStyle.heading3.copyWith(
+                  color: PuddingColor.brown,
+                ),
+              ),
+              PuddingElevatedButton(
+                onPressed: () {},
+                background: PuddingColor.main,
+                child: Text(
+                  '물어보러가기',
+                  style: PuddingTextStyle.body1.copyWith(
+                    color: PuddingColor.brown,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
