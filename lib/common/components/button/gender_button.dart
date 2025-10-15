444e7a68d739ea0894acd6e3758fd8316793323c
최승우 <choi0250655@gmail.com>
2025-10-15 10:46:00 +0900

    오타 수정
    오타 수정

--- lib/common/components/button/gender_button.dart
+++ lib/common/components/button/gender_button.dart
@@ -16,9 +16,9 @@ class PuddingGenderButton extends StatefulWidget {
 class _PuddingGenderButtonState extends State<PuddingGenderButton> {
    Gender? gender;
 
-  void selctedGender(Gender genderSelcet) {
+  void selectedGender(Gender genderSelecet) {
     setState(() {
-      gender = genderSelcet;
+      gender = genderSelecet;
       widget.onTap();
     });
   }
@@ -29,7 +29,7 @@ class _PuddingGenderButtonState extends State<PuddingGenderButton> {
     return Row(
       children: [
         ElevatedButton(
-          onPressed: () => selctedGender(Gender.man),
+          onPressed: () => selectedGender(Gender.man),
           style: ButtonStyle(
             padding: WidgetStateProperty.all(
               EdgeInsets.symmetric(vertical: 53, horizontal: 44),
@@ -52,7 +52,7 @@ class _PuddingGenderButtonState extends State<PuddingGenderButton> {
         ),
         SizedBox(width: 31),
         ElevatedButton(
-          onPressed: () => selctedGender(Gender.woman),
+          onPressed: () => selectedGender(Gender.woman),
           style: ButtonStyle(
             padding: WidgetStateProperty.all(
               EdgeInsets.symmetric(vertical: 53, horizontal: 44),
