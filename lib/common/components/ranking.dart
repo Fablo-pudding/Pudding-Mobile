c07665f62a0214ac523ac530ce97a431029aaead
최승우 <choi0250655@gmail.com>
2025-10-27 08:59:08 +0900

    pr 수정
    pr 수정

--- lib/common/components/ranking.dart
+++ lib/common/components/ranking.dart
@@ -7,42 +7,49 @@ class PuddingRanking extends StatelessWidget {
   final String? userImage;
   final String userName;
   final int puddingCount;
-  final Color backGroudColor;
+  final Color backgroudColor;
+
   const PuddingRanking({
     super.key,
     required this.ranking,
-     this.userImage,
+    this.userImage,
     required this.userName,
     required this.puddingCount,
-    required this.backGroudColor,
+    required this.backgroudColor,
   });
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.symmetric(vertical: 18,horizontal: 20),
-        color: backGroudColor,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Row(
-              children: [
-                Text(ranking.toString(), style: PuddingTextStyle.heading3),
-                const SizedBox(width: 18),
-                userImage != null ?  SvgPicture.asset(userImage!, width: 40, height: 40) : SvgPicture.asset('assets/img/profile.svg',width: 40,height: 40,),
-                SizedBox(width: 12),
-                Text(userName, style: PuddingTextStyle.heading3),
-              ],
-            ),
-            Row(
-              children: [
-                SvgPicture.asset('assets/img/pudding.svg', width: 40, height: 40),
-                SizedBox(width: 8),
-                Text('$puddingCount개',style: PuddingTextStyle.puddingCount,),
-              ],
-            ),
-          ],
-        ),
+      padding: EdgeInsets.symmetric(vertical: 18, horizontal: 20),
+      color: backgroudColor,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              Text(ranking.toString(), style: PuddingTextStyle.heading3),
+              const SizedBox(width: 18),
+              userImage != null
+                  ? SvgPicture.asset(userImage!, width: 40, height: 40)
+                  : SvgPicture.asset(
+                      'assets/img/profile.svg',
+                      width: 40,
+                      height: 40,
+                    ),
+              SizedBox(width: 12),
+              Text(userName, style: PuddingTextStyle.heading3),
+            ],
+          ),
+          Row(
+            children: [
+              SvgPicture.asset('assets/img/pudding.svg', width: 40, height: 40),
+              const SizedBox(width: 8),
+              Text('$puddingCount개', style: PuddingTextStyle.puddingCount),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
