42b97c8fc5bb38cbff57ee5ca179f0fa84dac93f
최승우 <choi0250655@gmail.com>
2025-10-28 08:17:34 +0900

    Ranking 만들기 (#30)
    * restRanking 구현

---
+++ lib/common/components/ranking.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:pudding/common/constants/text_style.dart';
+
+class PuddingRanking extends StatelessWidget {
+  final int ranking;
+  final String? userImage;
+  final String userName;
+  final int puddingCount;
+  final Color backgroudColor;
+
+  const PuddingRanking({
+    super.key,
+    required this.ranking,
+    this.userImage,
+    required this.userName,
+    required this.puddingCount,
+    required this.backgroudColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 18, horizontal: 20),
+      color: backgroudColor,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              Text(ranking.toString(), style: PuddingTextStyle.heading3),
+              const SizedBox(width: 18),
+              userImage != null
+                  ? SvgPicture.asset(userImage!, width: 40, height: 40)
+                  : SvgPicture.asset(
+                      'assets/img/profile.svg',
+                      width: 40,
+                      height: 40,
+                    ),
+              const SizedBox(width: 12),
+              Text(userName, style: PuddingTextStyle.heading3),
+            ],
+          ),
+          Row(
+            children: [
+              SvgPicture.asset('assets/img/pudding.svg', width: 40, height: 40),
+              const SizedBox(width: 8),
+              Text('$puddingCount개', style: PuddingTextStyle.puddingCount),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
