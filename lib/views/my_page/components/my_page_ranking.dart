9e10d492b502105f28f3cda67f2928b1e0ed8340
최승우 <choi0250655@gmail.com>
2025-11-05 23:01:00 +0900

    feat :: 마이 페이지 뷰 (#44)
    * my-page

---
+++ lib/views/my_page/components/my_page_ranking.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/cupertino.dart';
+import 'package:pudding/common/constants/color.dart';
+import 'package:pudding/common/constants/text_style.dart';
+
+class PuddingMyPageRanking extends StatelessWidget {
+  final int ranking;
+
+  const PuddingMyPageRanking({
+    super.key,
+    required this.ranking
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        padding: EdgeInsets.symmetric(vertical: 18, horizontal: 24),
+        decoration: BoxDecoration(
+          color: PuddingColor.main,
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text('👑랭킹', style: PuddingTextStyle.heading2),
+            Text('$ranking개', style: PuddingTextStyle.heading2),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/views/my_page/presentation/my_page.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:material_symbols_icons/symbols.dart';
+import 'package:pudding/common/components/app_bar/app_bar.dart';
+import 'package:pudding/common/constants/color.dart';
+import 'package:pudding/views/my_page/components/menu.dart';
+import 'package:pudding/views/my_page/components/my_page_ranking.dart';
+import 'package:pudding/views/my_page/components/profile.dart';
+import 'package:pudding/views/my_page/presentation/inquiry.dart';
+import 'package:pudding/views/my_page/presentation/log_out.dart';
+import 'package:pudding/views/my_page/presentation/notice.dart';
+
+class PuddingMyPage extends StatefulWidget {
+  const PuddingMyPage({super.key});
+
+  @override
+  State<PuddingMyPage> createState() => _PuddingMyPageState();
+}
+
+class _PuddingMyPageState extends State<PuddingMyPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: PuddingColor.background,
+      appBar: const PuddingAppBar(title: '마이페이지'),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            const SizedBox(height: 32,),
+            const PuddingProfile(
+              nickName: '최승우',
+              explanation: '대마고 1학년 2반 16번',
+            ),
+            const SizedBox(height: 48,),
+            const Row(
+              children: [
+                PuddingMyPageRanking(ranking: 3),
+              ],
+            ),
+            const SizedBox(height: 48,),
+            PuddingMenu(
+              menuName: '공지사항',
+              textColor: PuddingColor.black,
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const PuddingNotice(),
+                  ),
+                );
+                // todo 공지사항 넣기
+              },
+              icon: const Icon(Symbols.arrow_forward_ios),
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(12),
+                topRight: Radius.circular(12),
+              ),
+            ),
+            PuddingMenu(
+              menuName: '문의',
+              textColor: PuddingColor.black,
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const PuddingInquiry(),
+                  ),
+                );
+                //todo 문의 넣기
+              },
+              icon: const Icon(Symbols.arrow_forward_ios),
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const PuddingLogOut(),
+                  ),
+                );
+              },
+              child: const PuddingMenu(
+                menuName: '로그아웃',
+                textColor: PuddingColor.red,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(12),
+                  topRight: Radius.circular(12),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
