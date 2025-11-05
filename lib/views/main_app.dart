478c38ce1865280c6babbd5f28ea8d9352e33136
최승우 <choi0250655@gmail.com>
2025-11-05 14:18:00 +0900

    Squashed commit of the following:
    commit 3da98de3cc6b90e31fd208c88416cb5c09937643
    Author: 정지윤 <163826760+jiyun040@users.noreply.github.com>
    Date:   Wed Nov 5 09:23:11 2025 +0900
    
        feat :: 냉장고 페이지 구현 (#46)
    
        * feat :: 냉장고 아이템 표시 컴포넌트 추가 (#31)
    
        * 냉장고 아이템 표시 컴포넌트 추가
    
        * 사이즈 변수 추가 및 trailing comma 적용
    
        * feat :: 냉장고 전체 뷰 구현 및 화면 이동 수정 (#32)
    
        * 냉장고 전체 뷰 구현 및 화면 이동 수정
    
        * asset 클래스로 따로 분리
    
        * asset 클래스 적용

--- lib/views/main_app.dart
+++ lib/views/main_app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pudding/common/components/bottom_navigation_bar.dart';
+import 'package:pudding/views/fridge_page/presentation/fridge_page.dart';
 
 class PuddingMainApp extends StatefulWidget {
   const PuddingMainApp({super.key});
@@ -20,11 +21,13 @@ class _PuddingMainAppState extends State<PuddingMainApp> {
   ];
 
   void _onTap(int index) {
-    if (_currentIndex == index) {
-      _navigatorKeys[index].currentState!.popUntil((route) => route.isFirst);
-    } else {
-      _navigatorKeys[index].currentState!.popUntil((route) => route.isFirst);
-    }
+    setState(() {
+      if (_currentIndex == index) {
+        _navigatorKeys[index].currentState!.popUntil((route) => route.isFirst);
+      } else {
+        _currentIndex = index;
+      }
+    });
   }
 
   /// TODO 페이지 구현 시 Scaffold -> 페이지로 교체
@@ -52,7 +55,7 @@ class _PuddingMainAppState extends State<PuddingMainApp> {
           Navigator(
             key: _navigatorKeys[3],
             onGenerateRoute: (settings) =>
-                MaterialPageRoute(builder: (_) => Scaffold(body: Center(child: Text('냉장고 페이지'),),)),
+                MaterialPageRoute(builder: (_) => Scaffold(body: Center(child: Text('냉장고 페이지')),)),
           ),
           Navigator(
             key: _navigatorKeys[4],
