ebbb82b879985455f0163092ae84486eb63d5299
최승우 <choi0250655@gmail.com>
2025-11-29 23:08:23 +0900

    svg 추가
    svg 추가

--- lib/common/constants/pudding_assets.dart
+++ lib/common/constants/pudding_assets.dart
@@ -6,4 +6,5 @@ abstract final class PuddingAssets {
   static const logo = 'assets/img/logo.svg';
   static const crown = 'assets/img/crown.svg';
   static const profile = 'assets/img/profile.svg';
+  static const recipe = 'assets/img/recipe.svg';
 }
