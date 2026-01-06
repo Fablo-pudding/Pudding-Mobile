9ebc707c6f14407a50f31ecc5b589b5a3d55e311
leejia0324 <hx.un0707@gmail.com>
2026-01-06 19:36:52 +0900

    냉장고 model 제작
    storage_info, pudding_make, pudding_one_upgrade, pudding_two_upgrade, pudding_ingredient

---
+++ lib/common/models/storage_info.dart
@@ -0,0 +1,28 @@
+class StorageInfo {
+  final int milk;
+  final int sugar;
+  final int egg;
+  final int pudding1;
+  final int pudding2;
+  final int pudding3;
+
+  StorageInfo({
+    required this.milk,
+    required this.sugar,
+    required this.egg,
+    required this.pudding1,
+    required this.pudding2,
+    required this.pudding3,
+  });
+
+  factory StorageInfo.fromJson(Map<String, dynamic> json) {
+    return StorageInfo(
+        milk: json['milk'],
+        sugar: json['sugar'],
+        egg: json['egg'],
+        pudding1: json['pudding1'],
+        pudding2: json['pudding2'],
+        pudding3: json['pudding3']
+    );
+  }
+}
